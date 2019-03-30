e093e62971da19e7b48c776f54523c1c20980961
Juan Gomez <jg@juangomez.dev>
2019-03-29 18:36:38 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Container, Box, Button } from 'bloomer';
+import 'bulma/css/bulma.css';
+
+const Index = () => (
+  <Container>
+    <Box>Hello World!</Box>
+    <Button isColor='success' hasTextAlign='centered'>Hello!</Button>
+  </Container>
+)
+
+export default Index;
